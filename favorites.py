77e0bc30c1cb0dc396fe9141b4883fd9bdcec110
Clara <10088152+claraj@users.noreply.github.com>
2026-01-28 12:51:26 -0600

    Initial commit

---
+++ favorites.py
@@ -0,0 +1,101 @@
+"""A collaborative list of some of ITEC 2905's favorite things."""
+
+# 1. Favorite Color
+
+favorite_color = ''
+print(f'One of ITEC 2905s favorite colors is {favorite_color}')
+
+
+# 2. Favorite Food
+
+favorite_food = ''
+print(f'One of ITEC 2905s favorite foods is {favorite_food}')
+
+
+# 3. Favorite Movie or TV Show
+
+favorite_movie = ''
+print(f'One of ITEC 2905s favorite movies is {favorite_movie}')
+
+
+## 4. Favorite Music
+
+favorite_music = ''
+print(f'One of ITEC 2905s favorite types of music is {favorite_music}')
+
+
+## 5. Favorite Place
+
+favorite_place = ''
+print(f'One of ITEC 2905s favorite places is {favorite_place}')
+
+
+## 6. Favorite Holiday
+
+favorite_holiday = ''
+print(f'One of ITEC 2905s favorite holidays is {favorite_holiday}')
+
+## 7. Favorite Book
+
+favorite_book = ''
+print(f'One of ITEC 2905s favorite books is {favorite_book}')
+
+
+## 8. Favorite Animal
+
+favorite_animal = ''
+print(f'One of ITEC 2905s favorite animals is {favorite_animal}')
+
+
+## 9. Favorite Weather
+
+favorite_weather = ''
+print(f'One of ITEC 2905s favorite type of weather is {favorite_weather}')
+
+
+## 10. Favorite Drink
+
+favorite_drink = ''
+print(f'One of ITEC 2905s favorite drinks is {favorite_drink}')
+
+
+## 11. Favorite Store
+
+favorite_store = ''
+print(f'One of ITEC 2905s favorite stores is {favorite_store}')
+
+
+## 12. Favorite Sport
+
+favorite_sport = ''
+print(f'One of ITEC 2905s favorite sports is {favorite_sport}')
+
+
+## 13. Favorite Programming Language
+
+favorite_programming_language = ''
+print(f'One of ITEC 2905s favorite programming languages is {favorite_programming_language}')
+
+
+## 14. Favorite Mode of Transportation
+
+favorite_transportation = ''
+print(f'One of ITEC 2905s favorite modes of transportation is {favorite_transportation}')
+
+
+## 15. Favorite Website
+
+favorite_website = ''
+print(f'One of ITEC 2905s favorite websites is {favorite_website}')
+
+
+## 16. Favorite Season
+
+favorite_season = ''
+print(f'One of ITEC 2905s favorite seasons is {favorite_season}')
+
+
+## 17. Favorite Plant, Tree or Flower
+
+favorite_plant = ''
+print(f'One of ITEC 2905s favorite plants is {favorite_plant}')
