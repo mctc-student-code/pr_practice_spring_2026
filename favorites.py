1d723ca46fbb30573bcca641395e57039897ec0b
Zcaroll <qc0193ki@go.minneapolis.edu>
2026-01-28 13:52:42 -0600

    Added favorite programming language

--- favorites.py
+++ favorites.py
@@ -73,7 +73,7 @@ print(f'One of ITEC 2905s favorite sports is {favorite_sport}')
 
 ## 13. Favorite Programming Language
 
-favorite_programming_language = ''
+favorite_programming_language = 'Python'
 print(f'One of ITEC 2905s favorite programming languages is {favorite_programming_language}')
 
 
